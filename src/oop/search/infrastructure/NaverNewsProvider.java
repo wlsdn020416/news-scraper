d23a9c638280023603eaa8e07a771dedeb28e38a
wlsdn020416 <wlsdn020416@gmail.com>
2026-06-25 12:25:36 +0900

    NaverNewsProvider

---
+++ src/oop/search/infrastructure/NaverNewsProvider.java
@@ -0,0 +1,34 @@
+package oop.search.infrastructure;
+
+import oop.search.application.NewsProvider;
+import oop.search.domain.NewsCategory;
+import oop.search.domain.NewsResult;
+
+import java.net.http.HttpClient;
+import java.util.List;
+
+public class NaverNewsProvider extends AbstractHttpScraper{
+
+    private static  final String NEWS_API_URL = "https://openapi.naver.com/v1/search/news.json";
+
+    private final String clientId;
+    private final String clientSecret;
+    private final NewsCategory category;
+
+    public NaverNewsProvider() {
+        super(NEWS_API_URL);
+        this.clientId = System.getenv("NAVER_CLIENT_ID");
+        this.clientSecret = System.getenv("NAVER_CLIENT_SECRET");
+        this.category = NewsCategory.valueOf(System.getenv("NEWS_CATEGORY"));
+    }
+
+    @Override
+    public List<NewsResult> fetchNews(String searchQuery, int limit) {
+        return List.of();
+    }
+
+    public static void main(String[] args) {
+        NewsProvider provider = new NaverNewsProvider();
+        List<NewsResult> results = provider.fetchNews("팝업", 10);
+    }
+}
